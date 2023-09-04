bc27f3ffeaf82039be8dade33b20e359e7d7b7d1
JinJaeJee <jpluemphiromnat@gmail.com>
2023-09-04 18:53:30 +0700

    feat: add nav logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,20 @@
+import Logo from "../img/logo.jpg"
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
+          <img src={Logo} alt="logo" />
+        </div>
+        <div className="links">
+          <Link className="link" to="/?cat=art">
+            <h6>ART</h6>
+          </Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
