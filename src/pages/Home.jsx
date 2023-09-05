57faec2eb5274789160ba4b2553ffcaa668e36a3
JinJaeJee <jpluemphiromnat@gmail.com>
2023-09-05 16:37:48 +0700

    feat: update new logo pic, css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,46 @@
+import Discord from "../img/discordlogo.jpg"
+import Youtube from "../img/youtubelogo.png"
+import Facebook from "../img/facebooklogo.png"
+import "./home.css"
+
+
 const Home = () => {
+
+  const logoicons = [
+    {
+      id:1,
+      title:"Discord",
+      img:Discord,
+    },
+    {
+      id:2,
+      title:"Youtube",
+      img:Youtube
+    },
+    {
+      id:3,
+      title:"Facebook",
+      img:Facebook
+    }
+  ]
   return (
-    <div>Home</div>
+    <div className="home">
+      <div className="logologic">
+        {logoicons.map((icon) =>(
+          <div className="logolist" key={icon.id}>
+            <div className="img">
+              <img 
+                src={icon.img}
+                alt={icon.title}
+              />
+            </div>
+              <p>{icon.title}</p>
+          </div>
+        ))}
+      </div>
+
+
+    </div>
   )
 }
 
